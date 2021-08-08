87c3c4cdd3aed84efa7ab6731849f59b1275689d
abdhulla-k <abdhullak149@gmail.com>
2021-08-08 15:00:28 +0530

    solved a new problem in Array_proglems.py. modified existing function that_2

--- Array_problems.py
+++ Array_problems.py
@@ -2,7 +2,6 @@ Data = [1,2,6,7,4,0,5]                      # complexity Analysis
 Target = 10
 
 
-
 def test(self,Targe):
     i = 0
     j = 1
@@ -11,22 +10,51 @@ def test(self,Targe):
             if i + j == Target:
                 return i , j
 
+result = test(Data,Target) 
+print("from test: "+ str(result))
 
 
-def test2(Data,target):                     # in this case, O(n)T     O(n)S
+
+
+def test_2(Data,target):                     # in this case, O(n)T     O(n)S
     used = set()
     i = 0
     for i in range(len(Data)):
         num = Data[i]
         match = target - num
-        if used.__contains__(num):
+        if used.__contains__(match):
             return(num,match)
         else:
             used.add(num)
 
 
-result = test2(Data,Target)
-print(result)
 
-result = test(Data,Target)
-print(result)
+result2 = test_2(Data,Target)
+print("from test_2 : "+ str(result2))
+
+
+#===========================================================================================================
+
+
+Data_2 = [6,1,6,8,10,4,15,6,3,9,6]
+Target_2 = 6
+
+def test_3(Data_2,Target_2):
+    i = 0
+    temp = 0
+    for i in range(len(Data_2)):
+        if Data_2[i] == Target_2:
+            for j in range(len(Data_2)):
+                if i == j:
+                    break
+                elif Data_2[j] == Target_2:
+                    continue
+                else:
+                    temp = Data_2[i]
+                    Data_2[i] = Data_2[j]
+                    Data_2[j] = temp
+    return Data_2
+
+
+result_3 = test_3(Data_2, Target_2)
+print("from test_3: "+ str(result_3))
