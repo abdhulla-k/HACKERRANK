5105ca2232c54437b196ab61e96a1e55f144a688
abdhulla-k <abdhullak149@gmail.com>
2021-09-10 20:08:15 +0530

    completed function SolvMeFirst

---
+++ SolvMeFirst.py
@@ -0,0 +1,11 @@
+def solveMeFirst(a,b):
+    return(int(a)+int(b))
+    
+	# Hint: Type return a+b below
+
+
+
+num1 = int(input('Enter two number'))
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
