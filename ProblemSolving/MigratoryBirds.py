0c06a548d026acedfacfa5aa8a47ce441063e9b4
abdhulla-k <abdhullak149@gmail.com>
2021-11-11 17:05:49 +0530

    an anothor solution for 'Migratory Birds'

--- ProblemSolving/MigratoryBirds.py
+++ ProblemSolving/MigratoryBirds.py
@@ -15,6 +15,13 @@ def migratoryBirds(arr):
     Id = max(allOf)
     return allOf.index(Id)+1
 
+def Second(arr):
+    AllOf = [0]*5
+    for i in arr:
+        AllOf[i-1] += 1
+    return AllOf.index(max(AllOf))+1
+
 if __name__ == '__main__':
     arr = [1, 4, 4, 4, 5, 3]
     print(migratoryBirds(arr))
+    print(Second(arr))
