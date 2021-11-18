ae69c88bf09f8d718bb62f50b93737462eefc219
abdhulla-k <abdhullak149@gmail.com>
2021-11-18 20:52:44 +0530

    an another way to solve the problem 'Electronics Shop'

--- ProblemSolving/ElectronicsShop.py
+++ ProblemSolving/ElectronicsShop.py
@@ -8,8 +8,17 @@ def getMoneySpent(keyboards, drives, b):
     if max(k) == 0:
         return -1
 
+def second(keyboards, drives, b):
+    result = -1
+    for i in keyboards:
+        for j in drives:
+            if i+j <= b:
+                result = max(result, i+j)
+    return result
+
 if __name__ == '__main__':
     b = 10
     keyboards = [3, 1]
     drives = [5, 2, 8]
     print(getMoneySpent(keyboards, drives, b))
+    print(second(keyboards, drives, b))
