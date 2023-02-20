106ecd80d3c4ae6e2c94563edf5ac0df1a91aa9c
abdhulla-k <abdhullak149@gmail.com>
2023-02-20 15:20:11 +0530

    created a function to convert an array to heap

--- data_structures_algorithms/heap/min_heap.js
+++ data_structures_algorithms/heap/min_heap.js
@@ -1,90 +1,128 @@
-
-class MinHeap {
-
+class MaxHeap {
     constructor() {
-        // Initialing the array heap and adding a dummy element at index 0
-        this.heap = [null]
+        this.heap = [];
     }
 
-    getMin() {
-        //  Accessing the min element at index 1 in the heap array 
-        return this.heap[1]
+    printList() {
+        return this.heap;
     }
 
+    getLeftChildIndex(parentIndex) {
+        return 2 * parentIndex + 1;
+    }
 
-    insert(node) {
+    getRightChildIndex(parentIndex) {
+        return 2 * parentIndex + 2;
+    }
 
-        //  Inserting the new node at the end of the heap array 
-        this.heap.push(node)
+    getParentIndex(childIndex) {
+        return Math.floor((childIndex - 1) / 2);
+    }
 
-        //  Finding the correct position for the new node 
+    hasLeftChild(index) {
+        return this.getLeftChildIndex(index) < this.heap.length;
+    }
 
-        if (this.heap.length > 1) {
-            let current = this.heap.length - 1
+    hasRightChild(index) {
+        return this.getRightChildIndex(index) < this.heap.length;
+    }
 
-            //  Traversing up the parent node until the current node (current) is greater than the parent (current/2)
-            while (current > 1 && this.heap[Math.floor(current / 2)] > this.heap[current]) {
+    hasParent(index) {
+        return this.getParentIndex(index) >= 0;
+    }
 
-                //  Swapping the two nodes by using the ES6 destructuring syntax
-                [this.heap[Math.floor(current / 2)], this.heap[current]] = [this.heap[current], this.heap[Math.floor(current / 2)]]
-                current = Math.floor(current / 2)
-            }
+    leftChild(index) {
+        return this.heap[this.getLeftChildIndex(index)];
+    }
+
+    rightChild(index) {
+        return this.heap[this.getRightChildIndex(index)];
+    }
+
+    parent(index) {
+        return this.heap[this.getParentIndex(index)];
+    }
+
+    swap(indexOne, indexTwo) {
+        const temp = this.heap[indexOne];
+        this.heap[indexOne] = this.heap[indexTwo];
+        this.heap[indexTwo] = temp;
+    }
+
+    // get maximum element
+    peek() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+
+        return this.heap[0];
+    }
+
+    // remove from heap
+    poll() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+
+        const item = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+        this.heapifyDown();
+        return item;
+    }
+
+    // insert
+    add(item) {
+        this.heap.push(item);
+        this.heapifyUp();
+    }
+
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (this.hasParent(index) && this.parent(index) < this.heap[index]) {
+            this.swap(this.getParentIndex(index), index);
+            index = this.getParentIndex(index);
         }
     }
 
-    remove() {
-        //  Smallest element is at the index 1 in the heap array 
-        let smallest = this.heap[1]
-
-        //  When there are more than two elements in the array, we put the right most element at the first position
-        // and start comparing nodes with the child nodes
-
-        if (this.heap.length > 2) {
-            this.heap[1] = this.heap[this.heap.length - 1]
-            this.heap.splice(this.heap.length - 1)
-
-            if (this.heap.length === 3) {
-                if (this.heap[1] > this.heap[2]) {
-                    [this.heap[1], this.heap[2]] = [this.heap[2], this.heap[1]]
-                }
-                return smallest
-            }
-
-            let current = 1
-            let leftChildIndex = current * 2
-            let rightChildIndex = current * 2 + 1
-
-            while (
-                this.heap[leftChildIndex] &&
-                this.heap[rightChildIndex] &&
-                (this.heap[current] > this.heap[leftChildIndex] || this.heap[current] > this.heap[rightChildIndex])
+    heapifyDown() {
+        let index = 0;
+        while (this.hasLeftChild(index)) {
+            let largerChildIndex = this.getLeftChildIndex(index);
+            if (
+                this.hasRightChild(index) &&
+                this.rightChild(index) > this.leftChild(index)
             ) {
-
-                if (this.heap[leftChildIndex] < this.heap[rightChildIndex]) {
-                    [this.heap[current], this.heap[leftChildIndex]] = [this.heap[leftChildIndex], this.heap[current]]
-                    current = leftChildIndex
-                } else {
-                    [this.heap[current], this.heap[rightChildIndex]] = [this.heap[rightChildIndex], this.heap[current]]
-                    current = rightChildIndex
-                }
-
-                leftChildIndex = current * 2
-                rightChildIndex = current * 2 + 1
+                largerChildIndex = this.getRightChildIndex(index);
             }
+
+            if (this.heap[index] > this.heap[largerChildIndex]) {
+                break;
+            } else {
+                this.swap(index, largerChildIndex);
+            }
+
+            index = largerChildIndex;
         }
-
-        if (this.heap[rightChildIndex] === undefined && this.heap[leftChildIndex] < this.heap[current]) {
-            [this.heap[current], this.heap[leftChildIndex]] = [this.heap[leftChildIndex], this.heap[current]]
-        }
-
-        /* If there are only two elements in the array, we directly splice out the first element */
-
-        else if (this.heap.length === 2) {
-            this.heap.splice(1, 1)
-        } else {
-            return null
-        }
-
-        return smallest
     }
+
+    heapify(arr) {
+        // Start at the parent of the last element
+        const start = Math.floor((arr.length - 1) / 2);
+
+        // Perform the heapify operation on each parent node from the last to the first
+        for (let i = start; i >= 0; i--) {
+            heapifyDown(arr, i);
+        }
+    }
+
 }
+
+const heap = new MaxHeap();
+heap.add(3);
+heap.add(10);
+heap.add(22);
+heap.add(4);
+heap.add(2);
+heap.add(30);
+console.log(heap.printList());
