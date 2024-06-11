9ab5ee4299cec9d4fe0374c3a899e04a845f4e3f
abdhulla-k <abdhullak149@gmail.com>
2024-06-11 17:00:10 +0530

    leared three types of trtraversal through tree

--- data_structures_algorithms/tree/binary-search-all.js
+++ data_structures_algorithms/tree/binary-search-all.js
@@ -4,7 +4,7 @@ class BinarySearchTree {
     }
 
     // Insert a value to node
-    insert(value, root) {
+    insert(value, root = this.root) {
         if(this.root === null) {
             this.root = new Node(value);;
         } else {
@@ -23,6 +23,46 @@ class BinarySearchTree {
             }
         }
     }
+
+    preOrderTraversal(root = this.root) {
+        if(!root) {
+            return;
+        }
+
+        console.log(root.value);
+
+        if(root.left) {
+            this.preOrderTraversal(root.left);
+        }
+
+        if(root.right) {
+            this.preOrderTraversal(root.right);
+        }
+    }
+
+    inOrderTraversal(root = this.root) {
+        if(root.left) {
+            this.inOrderTraversal(root.left);
+        }
+
+        console.log(root.value);
+
+        if(root.right) {
+            this.inOrderTraversal(root.right);
+        }
+    }
+
+    postOrerTraversal(root = this.root) {
+        if(root.right) {
+            this.inOrderTraversal(root.right);
+        }
+
+        console.log(root.value);
+
+        if(root.left) {
+            this.inOrderTraversal(root.left);
+        }
+    }
 }
 
 class Node {
@@ -33,3 +73,17 @@ class Node {
     }
 }
 
+const tree = new BinarySearchTree();
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
+tree.insert(4);
+tree.insert(5);
+tree.insert(6);
+tree.insert(7);
+tree.insert(8);
+tree.insert(9);
+tree.insert(10);
+
+
+tree.preOrderTraversal();
